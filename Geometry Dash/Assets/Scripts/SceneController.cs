b9874424a040560619b110d631e13ce14258d0b0
DriveFallen <kobtsev2003@mail.ru>
2025-03-30 04:00:02 +0500

    перекидываю

---
+++ Geometry Dash/Assets/Scripts/SceneController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{  
+    public static void Restart()
+    {
+        int currentSceneID = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneID);
+    }
+
+    public static IEnumerator LoadNextScene()
+    {
+        int currentSceneID = SceneManager.GetActiveScene().buildIndex;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(currentSceneID + 1);
+    }
+
+    public void LoadLevel_1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadSceneByID(int ID)
+    {
+        SceneManager.LoadScene(ID);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
